0b1083a1992b14d3460d6f986b168e48dd24f651
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-04-04 22:31:52 +0300

    An other repeat maded successfully on use of controller streams

--- main.dart
+++ main.dart
@@ -4,17 +4,16 @@ StreamController<String> _controller = StreamController<String>.broadcast();
 Stream<String> get out => _controller.stream;
 
 void main() {
-  out.listen((data) => {
-        print(data),
-      });
+  out.listen((data) {
+    print(data);
+  });
 
-  out.listen((data) => {
-        print(data.replaceAll("v", "x")),
-      });
-
+  out.listen((data) {
+    print(data.replaceAll("e", "Y"));
+  });
   add();
 }
 
 void add() {
-  _controller.sink.add("bbbbvvv ged");
+  _controller.sink.add("Teeeest Chaine");
 }
