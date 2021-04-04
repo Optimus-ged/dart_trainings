c83123a2a0ae0f4ff8706653473127b17636f302
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-04-04 21:24:02 +0300

    Handling Expand in when using Streams

--- main.dart
+++ main.dart
@@ -3,24 +3,14 @@ void main() {
 }
 
 void listen() async {
-  // getNumbers().listen((data) {
-  //   print(data);
-  // });
-
-  var sum = 0;
-  getNumbers().listen((data) {
-    sum += data;
+  getNumbers().expand((data) => [data, data * 5]).listen((data) {
     print(data);
-  }).onDone(() {
-    print("Voici le total $sum");
   });
-
-  print("La longueur de notre Stream est de ${await getNumbers().length}");
 }
 
 Stream<int> getNumbers() async* {
-  for (var x = 1; x <= 3; x++) {
-    yield x;
+  for (var i = 1; i <= 3; i++) {
+    yield i;
     await Future.delayed(Duration(seconds: 1));
   }
 }
