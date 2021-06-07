dab2f6ac724adfcf1ba008c3774c69276aad2479
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-06-07 17:13:42 +0200

    Handling contains method when using list in dart

--- main.dart
+++ main.dart
@@ -4,15 +4,33 @@ StreamController<String> _controller = StreamController<String>.broadcast();
 Stream<String> get out => _controller.stream;
 
 void main() {
-  out.listen((data) {
-    print(data);
-  });
+  // out.listen((data) {
+  //   print(data);
+  // });
 
-  out.listen((data) {
-    print(data.replaceAll("a", "A"));
-  });
+  // out.listen((data) {
+  //   print(data.replaceAll("a", "A"));
+  // });
+
+  // int index = 4;
+  // bool isPair = (index % 2 == 0);
+  // print("Index $index");
+  // print("Le nombre est pair $isPair");
+
+  browseList();
+  print(browseList());
 }
 
 void add() {
   _controller.sink.add("aaaaa dd");
+}
+
+int browseList() {
+  int status = 0;
+  List data = [1, 2, 3, 4];
+  if (data.contains(10)) {
+    status = 200;
+  }
+  print(data);
+  return status;
 }
